aa49ca631b78fa7191e107f9acd25723bd7fd041
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2024-09-16 15:52:38 +0500

    test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"context"
 	"github.com/kbats183/simple-rtmp-restreamer/pkg/apiserver"
 	"github.com/kbats183/simple-rtmp-restreamer/pkg/registry"
 	"github.com/kbats183/simple-rtmp-restreamer/pkg/rtmpserver"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	setupLogger()
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	streamRegistry := registry.NewRegistry()
-	println("Starting...")
+	log.Println("Starting...")
 
 	rtmp := rtmpserver.NewMediaServer(rtmpserver.MediaServerConfig{}, streamRegistry)
 	web := apiserver.NewWebServer(streamRegistry)
-	go rtmp.Start()
-	web.Start()
+
+	go func() {
+		if err := rtmp.Start(ctx); err != nil {
+			log.Printf("RTMP server error: %v", err)
+			cancel()
+		}
+	}()
+
+	go func() {
+		if err := web.Start(ctx); err != nil {
+			log.Printf("API server error: %v", err)
+			cancel()
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the servers
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down servers...")
+
+	cancel()
+
+	rtmp.Stop()
+
+	if err := web.Stop(); err != nil {
+		log.Printf("Error stopping API server: %v", err)
+	}
+
+	log.Println("Servers stopped")
 }
--- pkg/apiserver/server.go
+++ pkg/apiserver/server.go
@@ -1,6 +1,7 @@
 package apiserver
 
 import (
+	"context"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kbats183/simple-rtmp-restreamer/pkg/registry"
@@ -11,6 +12,7 @@ import (
 type webServer struct {
 	registry registry.Registry
 	router   *chi.Mux
+	server   *http.Server
 }
 
 func NewWebServer(registry registry.Registry) *webServer {
@@ -21,14 +23,31 @@ func NewWebServer(registry registry.Registry) *webServer {
 
 	streamRouter := newStreamsRouter(router, registry)
 	streamRouter.Routes()
-	//router.Mount("/debug", middleware.Profiler())
+	router.Mount("/debug", middleware.Profiler())
 
 	return &webServer{
 		registry: registry,
 		router:   router,
+		server:   &http.Server{Addr: ":6070", Handler: router},
 	}
 }
 
-func (a *webServer) Start() {
-	log.Fatal(http.ListenAndServe(":6070", a.router)) //viper.GetString("server.port")
+func (a *webServer) Start(ctx context.Context) error {
+	go func() {
+		<-ctx.Done()
+		if err := a.Stop(); err != nil {
+			log.Printf("Error stopping web server: %v", err)
+		}
+	}()
+
+	log.Println("Starting web server on :6070")
+	if err := a.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	return nil
+}
+
+func (a *webServer) Stop() error {
+	log.Println("Stopping web server")
+	return a.server.Shutdown(context.Background())
 }
